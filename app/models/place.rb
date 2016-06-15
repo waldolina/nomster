5151aaf301647641c86bebf4140b89e83efce542
Erica Lai <ericalai89@gmail.com>
2016-06-15 04:02:44 +0000

    validate place name to require a name

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,7 +6,7 @@ class Place < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
   
-  validates :name, length: {minimum: 3}
+  validates :name, length: {minimum: 3}, presence: true
   validates :address, presence: true
   validates :description, presence: true
 end
